c29d0f4aef3e2b02bb86fa6398de3fe8c68a8c9e
Prabesh Shrestha <shresthaprabesh155@gmail.com>
2024-09-08 23:43:55 +0545

    Initial Commit

---
+++ MessagingApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MessagingApp
+//
+//  Created by Prabesh Shrestha on 08/09/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MessagingApp/MessagingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MessagingAppApp.swift
+//  MessagingApp
+//
+//  Created by Prabesh Shrestha on 08/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct MessagingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
